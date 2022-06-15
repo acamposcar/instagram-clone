000f8cda890edb86a70d9b409511858dd7302c1f
Alejandro Campos <acamposcar@gmail.com>
2022-06-15 16:31:33 +0200

    Add validate token route

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const isAuth = require('../middleware/auth').isAuth
 router.route('/login').post(userController.login)
 router.route('/register').post(userController.register)
 router.route('/avatar').post(isAuth, userController.uploadAvatar)
+router.route('/validate').get(isAuth, userController.validateToken)
 router.route('/accounts/:username').get(isAuth, userController.getProfile)
 
 module.exports = router
