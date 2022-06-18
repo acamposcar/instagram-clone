c3d849bb9baeb779b08ae183fb91a07d26276cbb
Alejandro Campos <acamposcar@gmail.com>
2022-06-18 16:51:09 +0200

    Add new header for post detail

--- client/src/pages/PostDetail.js
+++ client/src/pages/PostDetail.js
@@ -5,13 +5,13 @@ import Social from '../components/Post/Social'
 import DateFormat from '../components/Post/DateFormat'
 import Comments from '../components/Post/Comments'
 import CommentForm from '../components/Post/CommentForm'
-import Header from '../components/Post/Header'
 import useHttp from '../hooks/useHttp'
 import { getPost } from '../lib/api'
 import { useParams } from 'react-router-dom'
 import AlertError from '../components/AlertError'
 import PostSkeleton from '../components/Post/PostSkeleton'
 import useAuth from '../hooks/useAuth'
+import HeaderDetail from '../components/Post/HeaderDetail'
 
 const PostDetail = () => {
   const { postId } = useParams()
@@ -54,20 +54,23 @@ const PostDetail = () => {
       <Card width='auto'>
         <Flex flexDirection={{ base: 'column', md: 'row' }}>
           <Box maxWidth='550px'>
-            <Image borderRadius={{ md: '8px 0px 0px 8px', base: '8px 8px 0px 0px' }} minHeight={{ md: '600px', base: '300px' }} maxHeight='750px' height='100%' objectFit='cover' backgroundColor='black' objectPosition='center' src={post.image} alt='' />
+            <Image borderRadius={{ md: '8px 0px 0px 8px', base: '8px 8px 0px 0px' }} minHeight={{ md: '600px', base: '350px' }} maxHeight='750px' height='100%' objectFit='cover' backgroundColor='black' objectPosition='center' src={post.image} alt='' />
           </Box>
-          <Flex flexDirection='column' maxWidth={{ base: '550px', md: '385px' }} minWidth='280px' px={5} justifyContent='space-between'>
+          <Flex flexDirection='column' maxWidth={{ base: '550px', md: '385px' }} minWidth='280px' px={2} justifyContent='space-between'>
             <Box>
-              <Header user={post.author} location={post.location} avatarSize='lg' postId={post._id} />
+              <HeaderDetail user={post.author} location={post.location} avatarSize='lg' postId={post._id} />
               {/* CSS to hide scrollbar */}
-              <Box overflow='auto' css={{ msOverflowStyle: 'none', scrollbarWidth: 'none', '&::-webkit-scrollbar': { display: 'none' } }} maxHeight='350px'>
+              <Box overflow='auto' css={{ msOverflowStyle: 'none', scrollbarWidth: 'none', '&::-webkit-scrollbar': { display: 'none' } }} maxHeight='350px' px={5}>
                 <Text whiteSpace='pre-line' marginBottom={5} p={3}> {post.content}</Text>
                 {comments && <Comments postId={post._id} comments={comments} showViewAll={false} showAvatar />}
               </Box>
             </Box>
             <Box>
+
               <Social postId={post._id} likes={post.likes} saved={post.saved} />
-              <DateFormat date={post.createdAt} />
+              <Box px={5} my={3}>
+                <DateFormat date={post.createdAt} />
+              </Box>
               <hr />
               <CommentForm postId={post._id} onAddComment={handleAddComment} />
             </Box>
