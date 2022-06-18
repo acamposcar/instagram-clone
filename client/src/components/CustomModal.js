daf979de2b39b956cebf19eb630ed4f9048fa204
Alejandro Campos <acamposcar@gmail.com>
2022-06-18 12:48:32 +0200

    Add userList modal

--- client/src/components/CustomModal.js
+++ client/src/components/CustomModal.js
@@ -1,10 +1,10 @@
 import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody } from '@chakra-ui/react'
 
-const CustomModal = ({ title, children, isOpen, onClose }) => {
+const CustomModal = ({ title, children, isOpen, onClose, minW = '400px' }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
-      <ModalContent minW={{ md: '600px', base: '320px' }} borderRadius='8px'>
+      <ModalContent minW={{ md: { minW }, base: '320px' }} borderRadius='8px'>
         <ModalHeader textAlign='center' fontSize={15} borderBottom='1px solid var(--borderColor)'>{title}</ModalHeader>
         <ModalCloseButton />
         <ModalBody p={0}>
--- client/src/components/Post/LikedBy.js
+++ client/src/components/Post/LikedBy.js
@@ -1,24 +1,35 @@
 import React from 'react'
-import { Flex, Avatar, Text, Box } from '@chakra-ui/react'
+import { Flex, Avatar, Text, Box, useDisclosure } from '@chakra-ui/react'
 import RouterLink from '../RouterLink'
+import CustomModal from '../CustomModal'
+import UserList from '../UserList'
 const LikedBy = ({ likes }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   if (likes.length === 0) return (<></>)
 
   const othersLikes = likes.length > 1 ? <> and <Text as='span' fontWeight={500}>{likes.length - 1} others</Text></> : <></>
-
+  console.log(likes[0])
   return (
+    <>
+      <CustomModal isOpen={isOpen} onClose={onClose} title='Likes'>
+        <UserList list={likes} />
+      </CustomModal>
 
-    <Flex my={2} gap={2} px={5} alignItems='center'>
-      <RouterLink to={`/accounts/${likes[0].likedBy.username}`}>
-        <Avatar size='xs' name={likes[0].likedBy.username} src={`${likes[0].likedBy.avatar}`} />
-      </RouterLink>
-      <Box fontSize='14px'>Liked by
-        {' '}
-        <RouterLink to={`/accounts/${likes[0].likedBy.username}`}>{likes[0].likedBy.username}</RouterLink>
-        {' '}
-        {othersLikes}
-      </Box>
-    </Flex>
+      <Flex my={2} gap={2} px={5} alignItems='center'>
+        <RouterLink to={`/accounts/${likes[0].user.username}`}>
+          <Avatar size='xs' name={likes[0].user.username} src={`${likes[0].user.avatar}`} />
+        </RouterLink>
+        <Box fontSize='14px'>Liked by
+          {' '}
+          <RouterLink to={`/accounts/${likes[0].user.username}`}>{likes[0].user.username}</RouterLink>
+          {' '}
+          <Box as='button' onClick={onOpen}>
+            {othersLikes}
+          </Box>
+        </Box>
+      </Flex>
+    </>
   )
 }
 
