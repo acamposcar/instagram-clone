7abca02069a061f7c43cd1464d5d792af12bd744
Alejandro Campos <acamposcar@gmail.com>
2022-06-28 14:19:35 +0200

    Change authContext folder and file name

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import AuthContext from '../store/auth-context'
+import AuthContext from '../contexts/authContext'
 
 const useAuth = () => {
   return useContext(AuthContext)
--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import React, { StrictMode } from 'react'
 import * as ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthContextProvider } from './store/auth-context'
+import { AuthContextProvider } from './contexts/authContext'
 
 const container = document.getElementById('root')
 const root = ReactDOM.createRoot(container)
